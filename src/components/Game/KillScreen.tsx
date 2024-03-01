a8332e348cd144b67692eed2d08602b9221a5b4f
BigAwesome <12214194+BigAwesome@users.noreply.github.com>
2024-03-01 19:40:39 +0100

    some designs

--- src/components/Game/KillScreen.tsx
+++ src/components/Game/KillScreen.tsx
@@ -1,13 +1,23 @@
 import IKillScreenProps from "../../interfaces/IKillScreen";
+import ButtonImg from "../../assets/images/ButtonBG.png"
+import SnakeImg from "../../assets/images/Apple_1.png"
+import AppleImg from "../../assets/images/Apple_Red_1.png"
+
+
 
 function KillScreen(props: IKillScreenProps) {
     return <>
-        Game Over
-        <div onClick={() => {
-            props.setReset(false);
-            props.setTrys(props.trys + 1);
-        }}>REFLECT</div>
-        <div onClick={() => { }}>LEAVE</div>
+        <div className="ButtonWrapper" style={{ backgroundImage: `url(${ButtonImg})` }} >
+            <h2>Game Over</h2>
+            <div className="Button" onClick={() => {
+                props.setReset(false);
+                props.setTrys(props.trys + 1);
+            }}>
+                <img src={AppleImg} className="MenuIcon" />REFLECT</div>
+            <div className="Button" onClick={() => { window.location.reload() }}>
+                <img src={SnakeImg} className="MenuIcon" />LEAVE</div>
+        </div>
+
     </>
 }
 export default KillScreen
