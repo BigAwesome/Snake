2b07c41edbbc88cc2214d1069997a1490d83dd60
BigAwesome <12214194+BigAwesome@users.noreply.github.com>
2024-03-01 19:32:59 +0100

    Level 7 layout

--- src/components/Levels/Level7.tsx
+++ src/components/Levels/Level7.tsx
@@ -1,13 +1,21 @@
 import { useState } from "react"
 import { IGameProps } from "../../interfaces/IGame"
 import Video from "../Video";
+import ButtonImg from "../../assets/images/ButtonBG.png"
+
 
 function Level7(props: IGameProps) {
     const [video, setVideo] = useState(false);
     if (video) return <Video level={7} setVideo={(e) => { window.location.reload(); }} trys={0} game={props} />
     return <>
-        <button onClick={() => { window.location.reload() }}>RELIVE</button>
-        <button onClick={() => { setVideo(true) }}>QUIT</button>
+        <div className="ButtonWrapper" style={{ backgroundImage: `url(${ButtonImg})` }} >
+            <div className="Button ButtonWide" onClick={() => { window.location.reload() }}>
+                RELIVE</div>
+        </div >
+        <div className="ButtonWrapper" style={{ backgroundImage: `url(${ButtonImg})` }} >
+            <div className="Button ButtonWide" onClick={() => { setVideo(true) }}>
+                QUIT</div>
+        </div >
     </>
 }
 
