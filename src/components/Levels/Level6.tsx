db27436f6e05f039cab3afc34f36f9ad9ba7098b
BigAwesome <12214194+BigAwesome@users.noreply.github.com>
2024-03-05 21:09:15 +0100

    Format Level6

--- src/components/Levels/Level6.tsx
+++ src/components/Levels/Level6.tsx
@@ -6,7 +6,7 @@ import AppleImg from "../../assets/images/Apple_Black_1.png"
 
 function Level6(props: IGameProps) {
     return <>
-        <div className="ButtonWrapper">
+        <div className="ButtonWrapper6">
             <div className="Button" onClick={() => { if (props.onComplete) props.onComplete() }}>
                 <img src={DecoyImgRed} className="MenuIcon" />WIN</div>
             <div className="Button" onClick={() => { if (props.onComplete) props.onComplete() }}>
